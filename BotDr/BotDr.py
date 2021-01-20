b0d258c98b6a275aa6e1980042ea889df9c69526
Nikolay <botfgbartenevfgzero76@gmail.com>
2021-01-20 16:26:35 +0300

    Update BotDr.py

--- BotDr/BotDr.py
+++ BotDr/BotDr.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Copyright 2020 Nikolai Bartenev. Contacts: botfgbartenevfgzero76@gmail.com
+# Copyright 2021 Nikolai Bartenev. Contacts: botfgbartenevfgzero76@gmail.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
